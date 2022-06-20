89ddede9dfe49fbaa1667715ebcba7e5796eb08f
jake <jbudin@rockportllc.com>
2022-06-20 00:56:47 -0700

    Add readme

---
+++ main.py
@@ -0,0 +1,3 @@
+# 1. A trade executor... This code is like an API, you can talk to it and ask it to buy or sell some stocks or crypto for you. Its hooked up to exchanges, has API keys, and knows how much stocks/crypto you have. 
+# 2. A trade bot... This code like "makes decisions" in real time. Its deciding every second or every minute if it should buy or sell or do nothing. If it decides to buy and sell, it'll send a message to the trade executor to do something.
+# 3. A backtesting code. This code will accept various trade bots, and allow you to do parameterized back tests with different variables and let you compare and analyze the results to determine how good your strategy is..
